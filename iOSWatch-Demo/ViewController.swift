21cf497d9ce9bf32b96b252655900be130733bfe
Amrik <ztriplezero@hotmail.com>
2019-10-15 11:25:53 -0400

    Initial Commit

---
+++ iOSWatch-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSWatch-Demo
+//
+//  Created by Z Angrazy Jatt on 2019-10-15.
+//  Copyright © 2019 Z Angrazy Jatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
